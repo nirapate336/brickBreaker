f5cd65aeb9281c39d5f08886ef92546a799344fb
Nirav Patel <nirapate336@outlook.com>
2021-03-24 14:00:52 -0400

    Add project files.

---
+++ brickBreaker/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace brickBreaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("classic 10_65", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Beige;
+            this.titleLabel.Location = new System.Drawing.Point(121, 107);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(0, 51);
+            this.titleLabel.TabIndex = 0;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.AutoSize = true;
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("classic 10_65", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.subTitleLabel.Location = new System.Drawing.Point(88, 174);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(0, 28);
+            this.subTitleLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(600, 550);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Brick Breaker Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+    }
+}
+
---
+++ brickBreaker/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace brickBreaker
+{
+    public partial class Form1 : Form
+    {
+        //global lists
+        List<int> brickYList = new List<int>();
+        List<int> brickXList = new List<int>();
+
+
+
+        //global variables
+
+        int brickX = 30;
+        int brickY = 30;
+
+        int platformX = 240;
+        int platformY = 500;
+        int platformHeight = 20;
+        int platformWidth = 100;
+        int platformSpeed = 15;
+
+        int brickHeight = 20;
+        int brickWidth = 60;
+
+        int ballX = 395;
+        int ballY = 0;
+        int ballHeight = 5;
+        int ballWidth = 5;
+        int ballSpeed = 10;
+
+        int powerUp1X =200;
+        int powerUp1Y = 0;
+        int powerUp2X= 400;
+        int powerUp2Y = 0;
+
+        Random randGen = new Random();
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        bool leftDown = false;
+        bool rightDown = false;
+
+        int lives = 2;
+        int score = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 5; i++)
+            {
+                brickYList.Add(brickY);
+                brickXList.Add(brickX);
+                brickX = brickX + 160;
+
+
+            }
+
+            brickY = brickY + 100;
+            brickX = 30;
+
+            for (int n = 1; n < 5; n++)
+            {
+                brickYList.Add(brickY);
+                brickXList.Add(brickX);
+                brickX = brickX + 160;
+
+            }
+            brickY = brickY + 100;
+            brickX = 30;
+
+            for (int n = 1; n < 5; n++)
+            {
+                brickYList.Add(brickY);
+                brickXList.Add(brickX);
+                brickX = brickX + 160;
+
+
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+            }
+            }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, platformX, platformY, platformWidth, platformHeight);
+
+            for (int i = 0; i < brickYList.Count(); i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, brickXList[i], brickYList[i], brickWidth, brickHeight);
+            }
+
+
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            if (leftDown == true && platformX>0)
+            {
+                platformX -= platformSpeed;
+            }
+            if (rightDown == true&& platformX +platformWidth<this.Width)
+            {
+                platformX += platformSpeed;
+                
+            }
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Refresh();
+        }
+
+      
+    }
+}
